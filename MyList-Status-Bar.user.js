9fc0680ee5ff625f651edb3383b853db6b9fd878
wiidat <12127332+wiidat@users.noreply.github.com>
2020-04-03 01:52:29 +0300

    rise from grave
    working urls are fixed

--- MyList-Status-Bar.user.js
+++ MyList-Status-Bar.user.js
@@ -1,29 +1,21 @@
 // ==UserScript==
 // @name          MyList Status Bar
-// @version       3.0.1
+// @version       3.5
 // @author        wiidat
 // @description   UserScript for adding status bar to AniDB MyList page.
 // @license       GPL-3.0-or-later
-// @include       http://anidb.net/perl-bin/animedb.pl?*show=mylist*
-// @include       https://anidb.net/perl-bin/animedb.pl?*show=mylist*
-// @exclude       http://anidb.net/perl-bin/animedb.pl?*show=mylist*do=edit.state*
-// @exclude       http://anidb.net/perl-bin/animedb.pl?*show=mylist*do=cmp*
-// @exclude       http://anidb.net/perl-bin/animedb.pl?*show=mylist*do=mch*
-// @exclude       https://anidb.net/perl-bin/animedb.pl?*show=mylist*do=edit.state*
-// @exclude       https://anidb.net/perl-bin/animedb.pl?*show=mylist*do=cmp*
-// @exclude       https://anidb.net/perl-bin/animedb.pl?*show=mylist*do=mch*
+// @include       http://anidb.net/user/mylist/*
+// @include       https://anidb.net/user/mylist/*
 // @grant         none
-// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @icon          http://static.anidb.net/favicon.ico
 // @homepageURL	  https://github.com/wiidat/MyList-Status-Bar
-// @updateURL	  https://raw.githubusercontent.com/wiidat/MyList-Status-Bar/master/MyList-Status-Bar.user.js
-// @downloadURL	  https://raw.githubusercontent.com/wiidat/MyList-Status-Bar/master/MyList-Status-Bar.user.js
+// @namespace wiidat
 // ==/UserScript==
 
 var url = window.location.href;
 
-if (url.indexOf("show=mylist") > -1 && url.indexOf("show=mylist&do=add") == -1) {
-         var list_url = '//anidb.net/perl-bin/animedb.pl?show=mylist';
+if (url.indexOf("mylist") > -1 && url.indexOf("show=mylist&do=add") == -1) {
+         var list_url = '//anidb.net/user/mylist/';
          function getUrlVars() {
            var vars = {
            };
@@ -77,39 +69,39 @@ if (url.indexOf("show=mylist") > -1 && url.indexOf("show=mylist&do=add") == -1) 
          if (liststate_watching == 0) {
            var liststate_watching_url = ''
          } else {
-           var liststate_watching_url = '&liststate.watching=1'
+           var liststate_watching_url = '?do.update=1&liststate.watching=1'
          }
          if (liststate_completed == 0) {
            var liststate_completed_url = ''
          } else {
-           var liststate_completed_url = '&liststate.completed=1'
+           var liststate_completed_url = '?do.update=1&liststate.completed=1'
          }
          if (liststate_stalled == 0) {
            var liststate_stalled_url = ''
          } else {
-           var liststate_stalled_url = '&liststate.stalled=1'
+           var liststate_stalled_url = '?do.update=1&liststate.stalled=1'
          }
          if (liststate_dropped == 0) {
            var liststate_dropped_url = ''
          } else {
-           var liststate_dropped_url = '&liststate.dropped=1'
+           var liststate_dropped_url = '?do.update=1&liststate.dropped=1'
          }
          if (liststate_collecting == 0) {
            var liststate_collecting_url = ''
          } else {
-           var liststate_collecting_url = '&liststate.collecting=1'
+           var liststate_collecting_url = '?do.update=1&liststate.collecting=1'
          }
          if (liststate_unknown == 0) {
            var liststate_unknown_url = ''
          } else {
-           var liststate_unknown_url = '&liststate.unknown=1'
+           var liststate_unknown_url = '?do.update=1&liststate.unknown=1'
          }
          var a_anime = '<li><a href=\"' + list_url + user_url + '\">All Anime</a></li>'
-         var w_state = '<li><a href=\"' + list_url + user_url + '&liststate.watching=1\">Watching</a></li>'
-         var com_state = '<li><a href=\"' + list_url + user_url + '&liststate.completed=1\">Completed</a></li>'
-         var s_state = '<li><a href=\"' + list_url + user_url + '&liststate.stalled=1\">Stalled</a></li>'
-         var d_state = '<li><a href=\"' + list_url + user_url + '&liststate.dropped=1\">Dropped</a></li>'
-         var col_state = '<li><a href=\"' + list_url + user_url + '&liststate.collecting=1\">Plan to Watch</a></li>'
-         var u_state = '<li><a href=\"' + list_url + user_url + '&liststate.unknown=1\">Recently Added</a></li>'
+         var w_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.watching=1\">Watching</a></li>'
+         var com_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.completed=1\">Completed</a></li>'
+         var s_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.stalled=1\">Stalled</a></li>'
+         var d_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.dropped=1\">Dropped</a></li>'
+         var col_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.collecting=1\">Plan to Watch</a></li>'
+         var u_state = '<li><a href=\"' + list_url + user_url + '?do.update=1&liststate.unknown=1\">Recently Added</a></li>'
          $('ul.g_list').before('<ul class=\"g_list jump\">' + a_anime + w_state + com_state + s_state + d_state + col_state + u_state + '</ul>');
       }
